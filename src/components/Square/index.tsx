fd7af113581aefb09ea996326bdb47b9beab05b4
@krikun1983 <akhmadeevst@gmail.com>
2021-07-24 22:38:51 +0400

    feat: add square state x

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ValueSquareProps from '../../types/valueSquareProps';
 
 const Square = ({ value }: ValueSquareProps): JSX.Element => {
+  const [stateValue, setStateValue] = useState({ value: '' });
+
   return (
-    <button className="square" type="button">
-      {value}
+    <button className="square" type="button" onClick={() => setStateValue({ value: 'X' })}>
+      {stateValue.value}
     </button>
   );
 };
